1a048c447af929e5e045daf2cda313324ab47be8
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-15 12:20:19 -0400

    filter function

--- inventory-tracker/src/App.js
+++ inventory-tracker/src/App.js
@@ -20,10 +20,37 @@ function App() {
     console.log(data);
   };
 
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue
+      }
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue
+      }
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue
+      }
+
+      filteredData.push(item);
+    }
+
+    return filteredData;
+  }
+
   return (
     <div className="container">
       <div className="row mt-3">
-        <ItemsDisplay items={data["items"]}></ItemsDisplay>
+        <ItemsDisplay items={filterData(data["items"])}></ItemsDisplay>
       </div>
       <div className="row mt-3">
         <SearchBar updateSearchParams={updateFilters}></SearchBar>
