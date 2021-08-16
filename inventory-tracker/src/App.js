8dcfccf5b65cfd5ed0bd6a7ec35ea4c398800b51
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-15 22:55:34 -0400

    add load from json server

--- inventory-tracker/src/App.js
+++ inventory-tracker/src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import SearchBar  from './SearchBar';
 import AddItem from './AddItem';
 import ItemsDisplay from './ItemsDisplay';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
   const [filters, setFilters] = useState({});
   const [data, setData] = useState({items: []});
+
+  // Runs whenever data/filters updates
+  // useEffect(() => {
+  //   console.log("use effect")
+
+  //   return () => {
+  //     console.log("cleanuip")
+  //   }
+  // }, [data, filters])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items").then((response) => response.json()).then((data) => setData({items: data}))
+  }, []); // [] makes it run at start(mounted)
 
   const updateFilters = (searchParams) => {
     setFilters(searchParams);
@@ -14,10 +27,18 @@ function App() {
 
   const addItemToData = (item) => {
     let items = data["items"];
-    item.id = items.length;
-    items.push(item);
-    setData({items: items});
-    console.log(data);
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item)
+    };
+    fetch("http://localhost:3000/items", requestOptions).then((response) => response.json()).then((data) => {
+      items.push(data);
+      setData({items: items});
+    });
   };
 
   const filterData = (data) => {
@@ -58,6 +79,7 @@ function App() {
       <div className="row mt-3">
         <AddItem addItem={addItemToData}></AddItem>
       </div>
+      <br></br>
     </div>
   );
 }
